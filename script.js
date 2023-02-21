9af2781720e7942817c3ea4fb2ed493a1d907d00
hollycraig010 <126012409+hollycraig010@users.noreply.github.com>
2023-02-21 10:23:43 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+
+let firstCode = document.querySelector(".option-one-screen") ;
+let firstButton = document.querySelector(".option-one");
+
+firstButton.onclick=function(){
+firstCode.style.display="block";
+};
+
+
+let secondcode = document.querySelector(".option-two-screen") ;
+let secondbutton = document.querySelector(".option-two");
+secondbutton.onclick=function(){
+secondcode.style.display="block";
+};
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
